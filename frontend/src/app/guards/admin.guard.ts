33541853c478a17d54b5beb4cfe5616d8727e56e
Sofía Pinilla <contact@sofiapinilla.com>
2020-06-09 01:52:04 +0200

    bug fixed

--- frontend/src/app/guards/admin.guard.ts
+++ frontend/src/app/guards/admin.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable,OnInit } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
@@ -8,18 +8,25 @@ import { UserService } from '../services/user.service';
 })
 export class AdminGuard implements CanActivate {
   constructor(public userService:UserService,public router:Router) {}
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      const token: string = localStorage.getItem('authToken')
+  ngOnInit(): void {
+    const token: string = localStorage.getItem('authToken')
+    this.userService.getUserInfo(token)
+      .subscribe((res => {
+        this.userService.user = res
+    }))
+  }
+  canActivate(){
+    const token: string = localStorage.getItem('authToken')
     this.userService.getUserInfo(token)
     if (this.userService['user']['role']=='admin'){
       return true;
-    } else {
+    } 
       this.router.navigate(['login']);
       return false;
-    }
+   
   }
+  
+  
   
   
 }
